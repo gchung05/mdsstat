3c5daf2b06da7f3c4c696416261cb12e9d2b2cc6
Gary Chung <gchung05@gmail.com>
2019-01-14 20:58:05 -0800

    fixed source data

--- R/sprt.R
+++ R/sprt.R
@@ -127,11 +127,7 @@
 #'
 #' @examples
 #' # Basic Example
-#' data <- data.frame(time=c(1:25),
-#'                    nA=as.integer(stats::rnorm(25, 25, 5)),
-#'                    nB=as.integer(stats::rnorm(25, 50, 5)),
-#'                    nC=as.integer(stats::rnorm(25, 100, 25)),
-#'                    nD=as.integer(stats::rnorm(25, 200, 25)))
+#' data <- data.frame(time=c(1:25), event=as.integer(stats::rnorm(25, 100, 25)))
 #' a1 <- sprt(data)
 #' # Example using an mds_ts object
 #' a2 <- sprt(mds_ts[[3]])
